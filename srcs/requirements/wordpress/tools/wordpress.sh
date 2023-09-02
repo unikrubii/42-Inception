9eef1111e68eb01fefd0e41445e941f83bb49ab5
Sirilert Thitikul <rubii@Rubii-Mac.local>
2023-09-02 18:49:13 +0700

    DB DB

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -20,7 +20,7 @@ else
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
+	sed -i "s/database_name_here/$MYSQL_DB/g" wp-config.php
 
 	wp user create $WORDPRESS_ADMIN_USER $WORDPRESS_ADMIN_EMAIL --role=administrator --user_pass=$WORDPRESS_ADMIN_PASSWORD --allow-root
 	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
