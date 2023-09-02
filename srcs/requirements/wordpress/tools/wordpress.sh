3c5b6a438aaff1554b6ec616b6642111cb913634
Sirilert Thitikul <rubii@Rubii-Mac.local>
2023-09-02 17:26:52 +0700

    fix

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -16,11 +16,11 @@ else
 	rm -rf wordpress
 
 	#Inport env variables in the config file
+	cp wp-config-sample.php wp-config.php
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
-	cp wp-config-sample.php wp-config.php
 
 	wp user create $WORDPRESS_ADMIN_USER $WORDPRESS_ADMIN_EMAIL --role=administrator --user_pass=$WORDPRESS_ADMIN_PASSWORD --allow-root
 	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
