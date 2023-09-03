96177b8f7cfdb2772acf18b1f7e3a9ba55b0ebd8
Sirilert Thitikul <rubii@Rubii-Mac.local>
2023-09-03 16:00:08 +0700

    change env value in wordpress.sh

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -17,10 +17,10 @@ else
 
 	#Inport env variables in the config file
 	cp wp-config-sample.php wp-config.php
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config.php
-	sed -i "s/database_name_here/$MYSQL_DB/g" wp-config.php
+	sed -i "s/username_here/$WORDPRESS_DB_USER/g" wp-config.php
+	sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" wp-config.php
+	sed -i "s/localhost/$WORDPRESS_DB_HOST/g" wp-config.php
+	sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g" wp-config.php
 
 	wp user create $WORDPRESS_ADMIN_USER $WORDPRESS_ADMIN_EMAIL --role=administrator --user_pass=$WORDPRESS_ADMIN_PASSWORD --allow-root
 	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
